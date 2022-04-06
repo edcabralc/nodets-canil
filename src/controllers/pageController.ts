0e0b1931e599a9c6866e7440b2bdf2aeb1aaf39d
unknown <cabralnet@gmail.com>
2022-04-06 12:56:52 -0300

    add helpers folder

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,18 +1,43 @@
 import {Request, Response} from "express";
+import {createMenuOptions} from "../helpers/createMenuObject";
 
 export const home = (req: Request, res: Response) => {
-  res.send(`Servidor rodando na porta ${process.env.PORT}`);
-  // res.render('pages/homes')
+  // res.send("pages/home");
+  res.render("pages/page", {
+    menu: createMenuOptions("all"),
+    banner: {
+      title: "Todos os animais",
+      background: "allanimals.jpg",
+    },
+  });
 };
 export const cats = (req: Request, res: Response) => {
-  res.send("Aqui você encontra gatos");
-  // res.render('pages/homes')
+  // res.send("pages/cats");
+  res.render("pages/page", {
+    menu: createMenuOptions("cat"),
+    banner: {
+      title: "Gatos",
+      background: "banner_cat.jpg",
+    },
+  });
 };
 export const dogs = (req: Request, res: Response) => {
-  res.send("Aqui você encontra cachorros");
+  res.render("pages/page", {
+    menu: createMenuOptions("dog"),
+    banner: {
+      title: "Cachorros",
+      background: "banner_dog.jpg",
+    },
+  });
   // res.render('pages/homes')
 };
 export const fishes = (req: Request, res: Response) => {
-  res.send("Aqui você encontra peixes");
+  res.render("pages/page", {
+    menu: createMenuOptions("fish"),
+    banner: {
+      title: "Peixes",
+      background: "banner_fish.jpg",
+    },
+  });
   // res.render('pages/homes')
 };
